91223696ee8aadf5aaa3dc1c973233b6aed5b043
Solmoon21 <rpob716@gmail.com>
2023-09-20 11:02:38 +0200

    Update

--- src/DataManager.js
+++ src/DataManager.js
@@ -1,13 +1,7 @@
 import firebase from "firebase"
 
 firebase.initializeApp({
-    apiKey: "AIzaSyAty9oiWEgRVNtJw88h318eW7HEr3QRbTQ",
-    authDomain: "lottery-dd6cf.firebaseapp.com",
-    projectId: "lottery-dd6cf",
-    storageBucket: "lottery-dd6cf.appspot.com",
-    messagingSenderId: "545206738754",
-    appId: "1:545206738754:web:c6bac779d12cf962ce32db",
-    measurementId: "G-L8FHS14THJ"
+    /* credentials */
 });
 
 var db = firebase.database();
